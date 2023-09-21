8023938b176b6d0696fb3577e36a0a0e98a8b384
Andrew <zhutavlyov@gmail.com>
2023-09-21 15:30:21 +0300

    Добавьте файлы проекта.

---
+++ genkrv/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace genkrv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new System.Windows.Forms.CheckBox();
+            checkBox2 = new System.Windows.Forms.CheckBox();
+            checkBox3 = new System.Windows.Forms.CheckBox();
+            checkBox4 = new System.Windows.Forms.CheckBox();
+            checkBox5 = new System.Windows.Forms.CheckBox();
+            checkBox6 = new System.Windows.Forms.CheckBox();
+            checkBox7 = new System.Windows.Forms.CheckBox();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(12, 12);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(79, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Латиница";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(12, 37);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(88, 19);
+            checkBox2.TabIndex = 0;
+            checkBox2.Text = "Кириллица";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new System.Drawing.Point(12, 62);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new System.Drawing.Size(67, 19);
+            checkBox3.TabIndex = 0;
+            checkBox3.Text = "Цифры";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new System.Drawing.Point(12, 87);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new System.Drawing.Size(156, 19);
+            checkBox4.TabIndex = 0;
+            checkBox4.Text = "Специальные символы";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new System.Drawing.Point(12, 112);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new System.Drawing.Size(82, 19);
+            checkBox5.TabIndex = 0;
+            checkBox5.Text = "Строчные";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new System.Drawing.Point(12, 137);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new System.Drawing.Size(85, 19);
+            checkBox6.TabIndex = 0;
+            checkBox6.Text = "Заглавные";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new System.Drawing.Point(12, 162);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new System.Drawing.Size(113, 19);
+            checkBox7.TabIndex = 0;
+            checkBox7.Text = "Без повторений";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(12, 206);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(12, 235);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(12, 264);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new System.Drawing.Size(513, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(12, 309);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(181, 35);
+            button1.TabIndex = 2;
+            button1.Text = "Сгенерировать пароль";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(253, 309);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(181, 35);
+            button2.TabIndex = 2;
+            button2.Text = "Проверить введенный пароль";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(118, 209);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(129, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Задайте длину пароля";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(118, 238);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(171, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Введите пароль для проверки";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(531, 267);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(49, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Пароль";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(12, 353);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(38, 15);
+            label4.TabIndex = 3;
+            label4.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(580, 377);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(checkBox7);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox6);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
