b624ffe47c1416fc79f844940f0254dfb830aea4
Andrew <zhutavlyov@gmail.com>
2023-09-22 22:20:17 +0300

    Дописан генератор

--- genkrv/Generator.cs
+++ genkrv/Generator.cs
@@ -3,49 +3,12 @@ using System.CodeDom;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Forms;
 
 namespace genkrv
 {
     internal class Generator
-    {/*
-        //Todo разбить алфавиты на несколько
-        /*
-        private char[] alph = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-            'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я',
-            'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ь','ы','э','ю','я',
-            '0','1','2','3','4','5','6','7','8','9',
-            '!','#','$','%','&','(',')','*','+','-','=','.',',','/','<','>','?','@','[',']','^','_','`','{','}','|','~'};
-        
-        //Todo придумать алгоритм переключения между алфавитами в зависимости от сета правил
-        //Todo алгоритм рассчета алфавита в зависимости от правил
-        //Стринговый алфавит
-        //Стандартный алфавит и массив рабочих значений
-        private char[] latinaL = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        private char[] latinaU = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-        private char[] cyrL = new char[] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'ы', 'э', 'ю', 'я' };
-        private char[] cyrU = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
-        private char[] digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        private char[] spec = new char[] { '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '=', '.', ',', '/', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~' };
-
-        public int getAphabetLength() {
-            return alph.Length;
-        }
-        //Todo добавить механизм слияния алфавитов
-        //Todo добавить механизм генерации без повторов
-        public string generatePassword(int length) {
-            StringBuilder password = new StringBuilder();
-            Random random = new Random();
-            for (int i = 0; i < length; i++) {
-                int index = random.Next(length - 1);
-                password.Append(alph[index]);
-            }
-            return password.ToString();
-        }
-        */
-
-        //Todo провести тесты
-        //Todo свзяать с формой
+    {
         private const string latinaL = "abcdefghijklmnopqrstuvwxyz";
         private const string latinaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private const string cyrL = "абвгдеёжзийклмнопрстуфхцчшщъьыэюя";
@@ -57,21 +20,20 @@ namespace genkrv
 
         private string alphabet = "";
 
+        private bool noRepeats = false;
+
         public byte alphabetLength() { 
             return Convert.ToByte(alphabet.Length);
         }
         private void generateAlphabet(byte rule) {
             alphabet = "";
-            byte[] rules = new byte[] { 1, 2, 4, 8, 16, 32 };
-            while (rule > 0) {
+            byte[] rules = new byte[] { 1, 2, 4, 8, 16, 32, 64 };
+            short ruleSet = rule;
+            while (ruleSet > 0) {
                 for (int i = rules.Length - 1; i >= 0; i--)
                 {
-                    if (rules[i] > rule) rules[i] = 0;
-                    if (rules[i] <= rule)
-                    {
-                        rule -= rules[i];
-                        //continue;
-                    }
+                    if (rules[i] > ruleSet) rules[i] = 0;
+                    if (rules[i] <= ruleSet) ruleSet -= rules[i];
                 }
             }
                 for (int i = 0; i < rules.Length; i++)
@@ -112,28 +74,38 @@ namespace genkrv
                         case 8:
                             alphabet += spec;
                             break;
+                        case 64:
+                            noRepeats = true;
+                        break;
                         default:
                             break;
                     }//Конец свича
-                }//Конец 
+                }//Конец цикла формирования алфавита
         }//Конец метода
 
-        public string generatePassword(short length, bool noRepeats, byte rule) {
+        public string generatePassword(short length, byte rule) {
             generateAlphabet(rule);
+            if (length > alphabetLength()) length = alphabetLength();
             StringBuilder password = new StringBuilder();
             Random random = new Random();
             if (noRepeats) {
+                HashSet<char> usedChars = new HashSet<char>();
                 for (int i = 0; i < length; i++)
                 {
-                    int index = random.Next(alphabetLength() - 1);
-                    password.Append(alphabet[index]);
-                    alphabet = alphabet.Remove(alphabet[index], 1);
+                    int index = random.Next(alphabetLength());
+                    char nextChar = alphabet[index];
+                    if (!usedChars.Contains(nextChar))
+                    {
+                        password.Append(alphabet[index]);
+                        usedChars.Add(nextChar);
+                    }
                 }
             }
             else { 
             for (int i = 0; i < length; i++)
             {
-                int index = random.Next(alphabetLength() - 1);
+                int index = random.Next(alphabetLength());
+                char nextChar = alphabet[index];
                 password.Append(alphabet[index]);
             } 
         }
